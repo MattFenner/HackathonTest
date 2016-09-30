6adb2cd62b0c4c7f735d6d8ea78b5d2be1bd1bad
Matthew Fenner <matthew.fenner@transmax.com.au>
2016-09-30 19:09:32 +1000

    first commit

---
+++ src/Aggregators/AggregatorTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregatorTest
+{
+    public class Class1
+    {
+    }
+}
