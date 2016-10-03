39e69c42679ecb10dee71236c902ceabcdbb1aec
MattMon <mattjfenner@gmail.com>
2016-10-03 21:28:50 +1000

    working on buildscripts

--- src/Aggregators/AggregatorTest/buildAndDeploy.ps1
+++ src/Aggregators/AggregatorTest/buildAndDeploy.ps1
@@ -9,5 +9,10 @@ echo "--------------"
 echo "building $name"
 Invoke-Expression "& 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe' '$folder/$name.csproj' /p:Configuration=Release"
 
-#deploy
+#create deploy revision
+echo "creating deployment revision"
+Copy-Item -Path "$folder/appspec.yml" -Destination "$folder/bin/Release"
 Invoke-Expression "& aws deploy push --application-name $name --s3-location s3://mfdeploytest/$name.zip --source '$folder/bin/Release'"
+
+echo "deploying $name"
+Invoke-Expression "& aws deploy create-deployment --application-name $name --s3-location bucket=mfdeploytest,key=AggregatorTest.zip,bundleType=zip --deployment-group-name $name"
