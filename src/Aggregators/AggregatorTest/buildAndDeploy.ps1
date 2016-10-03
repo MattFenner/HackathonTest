05bd98be2aa34c49c8e44135bf20a8df15eef641
MattMon <mattjfenner@gmail.com>
2016-10-03 21:10:05 +1000

    working on scripts

--- src/Aggregators/AggregatorTest/buildAndDeploy.ps1
+++ src/Aggregators/AggregatorTest/buildAndDeploy.ps1
@@ -10,4 +10,4 @@ echo "building $name"
 Invoke-Expression "& 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe' '$folder/$name.csproj' /p:Configuration=Release"
 
 #deploy
-Invoke-Expression "& aws deploy push --application-name $name --s3-location s3://mfdeploytest/$name.zip --source bin/Release"
+Invoke-Expression "& aws deploy push --application-name $name --s3-location s3://mfdeploytest/$name.zip --source '$folder/bin/Release'"
